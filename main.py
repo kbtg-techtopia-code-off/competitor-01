f5a8a921f66888e2c1cfd30d2476bea6372b96ac
kbtgtechtopia01 <kbtgtechtopia01@kasikornbank.com>
2023-09-01 14:56:39 +0700

    C1

---
+++ main.py
@@ -0,0 +1,53 @@
+
+
+# Path: main.py
+# // create command line game rock paper scissors with python
+# // and the game should be game loop and count gampe played and print game winds
+
+import random
+
+wins = 0
+losses = 0
+played = 0
+
+def play():
+    user = input("Please input your choice: ")
+    computer = random.choice(['rock','paper','scissor'])
+    print(f"The computer choice : {computer}")
+    print(f"The player choice : {user}")
+
+    if user == computer:
+        return 'It\'s a tie'
+
+    # r > s, s > p, p > r
+    if is_win(user, computer):
+        return 'P'
+    return 'C'
+
+def is_win(player, opponent):
+    # return true if player wins
+    # r > s, s > p, p > r
+    if (player == 'rock' and opponent == 'scissor') or (player == 'scissor' and opponent == 'paper') \
+        or (player == 'paper' and opponent == 'rock'):
+        return True
+
+while True:
+    result = play()
+    if result == 'P':
+        print('The winer is player')
+        wins += 1
+    elif result == 'C':
+        print('The winer is computer')
+        losses += 1
+    
+    played += 1
+    print(f"Played: {played}")
+    print(f"Wins: {wins}")
+    print(f"Losses: {losses}")
+    print("==================")
+
+    if played == 99:
+        break
+
+
+
